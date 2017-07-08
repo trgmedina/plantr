f6cecdf66af69cefb8a3d3c43766f164317036d6
Theresa Medina <trgmedina@gmail.com>
2017-07-08 11:09:05 -0500

    set up plant schema for mongo db

---
+++ models/plant.js
@@ -0,0 +1,38 @@
+// Include the Mongoose Dependencies
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+// Create a Schema for adding plants. 
+var PlantSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+  	type: String
+  },
+  origin: {
+  	type: String
+  },
+  sunlightAmt: {
+  	type: String,
+  	required: true
+  },
+  waterSchedule: {
+  	type: String,
+  	required: true
+  },
+  height: {
+  	type: String
+  },
+  specialCare: {
+  	type: String
+  }
+});
+
+// Create the Model
+var Plant = mongoose.model("Plant", PlantSchema);
+
+// Export it for use elsewhere
+module.exports = Plant;
