cf11dd13192a7aea7fc9fd94d296c87c34871220
Theresa Medina <trgmedina@gmail.com>
2017-07-08 11:11:03 -0500

    set up server.js

---
+++ server.js
@@ -0,0 +1,41 @@
+// Include Server Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Require Plant schema
+var Plant = require("./models/plant");
+
+// Create a new express app
+var app = express();
+// Sets an initial port. We'll use this later in our listener
+var PORT = process.env.PORT || 3000;
+
+// Run Morgan for Logging
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(express.static("./public"));
+
+// -------------------------------------------------
+
+// MongoDB configuration (Change this URL to your own DB)
+mongoose.connect("mongodb://localhost/plantapp");
+var db = mongoose.connection;
+
+db.on("error", function(err) {
+  console.log("Mongoose Error: ", err);
+});
+
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+// Starting our express server
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
