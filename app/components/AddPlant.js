d9b6ca82b5583a48b9136947d2420e4aab42cf46
meeshyd <michellemdidier@gmail.com>
2017-07-09 09:40:08 -0500

    add plant form updates

--- app/components/AddPlant.js
+++ app/components/AddPlant.js
@@ -16,108 +16,99 @@ var AddPlant = React.createClass({
               </div>
           </div>
           <div className="row">
-              <div className="col-xs-12">
+              <div className="col-xs-8 col-xs-offset-2">
                 <form>
-                  <div className="form-group">
-                    <label for="plantName">Name of Plant</label>
-                    <input type="text" class="form-control" id="name-input" aria-describedby="name" placeholder="Name" required></input>
+                  <div id="addPlant-search">
+                      <div className="input-group">
+                        <input type="text" className="form-control" placeholder="Search for Plant Name..."></input>
+                        <span className="input-group-btn">
+                          <button className="btn btn-default" type="button">Go!</button>
+                        </span>
+                      </div>
                   </div>
-                  <div className="form-group">
-                    <label for="nickname">Nickname</label>
-                    <input type="text" class="form-control" id="nickname-input" aria-describedby="nickname" placeholder="Nickname"></input>
-                    <small id="nickname-help" class="form-text text-muted">We'll use this name for your plant if you prefer.</small>
-                  </div>
-                  <div className="form-group">
-                    <label for="plant-image">Image URL</label>
-                    <input type="text" class="form-control" id="image-input" aria-describedby="image" placeholder="Nickname" required></input>
-                  </div>
-                  <div className="form-group">
-                    <label for="description">Description</label>
-                    <textarea className="form-control" id="description-input" rows="3"></textarea>
-                  </div>
-                  <div className="form-group">
-                    <label for="origin">Plant Origin</label>
-                    <input type="text" class="form-control" id="origin-input" aria-describedby="origin" placeholder="Origin"></input>
-                  </div>
-                  <div className="form-group">
-                    <label for="light">Preferred Light</label>
-                    <select className="form-control" id="light-input">
-                      <option>Full Sun</option>
-                      <option>Partial Sun</option>
-                      <option>Bright Light</option>
-                      <option>Shade</option>
-                    </select>
-                  </div>
-                  <div className="form-group">
-                    <label for="exampleSelect2">Watering Frequency</label>
-                    <select className="form-control" id="water-input">
-                      <option>Once a Day</option>
-                      <option>Once a Week</option>
-                      <option>Every Other Week</option>
-                      <option>Once a Month</option>
-                      <option>Infrequent</option>
-                    </select>
-                  </div>
-                  <fieldset className="form-group">
-                    <legend>Create an alert</legend>
-                    <div className="form-check">
-                      <label className="form-check-label">
-                        <input type="checkbox" className="form-check-input" name="optionsCheck" id="sunday" value="Su"></input>
-                        Su
-                      </label>
+                  <div id="addPlant-remaining">
+                    <div className="form-group">
+                      <label htmlFor="nickname">Nickname</label>
+                      <input type="text" className="form-control" id="nickname-input" aria-describedby="nickname" placeholder="Nickname"></input>
+                      <small id="nickname-help" className="form-text text-muted">We'll use this name for your plant if you prefer.</small>
                     </div>
-                    <div className="form-check">
-                    <label className="form-check-label">
-                        <input type="checkbox" className="form-check-input" name="optionsCheck" id="monday" value="monday"></input>
-                        M
-                      </label>
+                    <div className="form-group">
+                      <label htmlFor="plant-image">Image URL</label>
+                      <input type="text" className="form-control" id="image-input" aria-describedby="image" placeholder="Nickname" required></input>
                     </div>
-                    <div className="form-check">
-                      <label className="form-check-label">
-                        <input type="checkbox" className="form-check-input" name="optionsCheck" id="tuesday" value="tuesday"></input>
-                        Tu
-                      </label>
+                    <div className="form-group">
+                      <label htmlFor="description">Description</label>
+                      <textarea className="form-control" id="description-input" rows="3"></textarea>
                     </div>
-                    <div className="form-check">
-                    <label className="form-check-label">
-                        <input type="checkbox" className="form-check-input" name="optionsCheck" id="wednesday" value="wednesday"></input>
-                        W
-                      </label>
+                    <div className="form-group">
+                      <label htmlFor="origin">Plant Origin</label>
+                      <input type="text" className="form-control" id="origin-input" aria-describedby="origin" placeholder="Origin"></input>
                     </div>
-                    <div className="form-check">
-                      <label className="form-check-label">
-                        <input type="checkbox" className="form-check-input" name="optionsCheck" id="thursday" value="thursday"></input>
-                        Th
-                      </label>
+                    <div className="row">
+                      <div className="col-md-6 col-xs-12">
+                        <div className="form-group">
+                          <label htmlFor="light">Preferred Light</label>
+                          <select className="form-control" id="light-input">
+                            <option>Full Sun</option>
+                            <option>Partial Sun</option>
+                            <option>Bright Light</option>
+                            <option>Shade</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div className="col-md-6 col-xs-12">
+                        <div className="form-group">
+                          <label htmlFor="water">Preferred Watering Frequency</label>
+                          <select className="form-control" id="water-input">
+                            <option>Once a Day</option>
+                            <option>Once a Week</option>
+                            <option>Every Other Week</option>
+                            <option>Once a Month</option>
+                            <option>Infrequent</option>
+                          </select>
+                        </div>
+                      </div>
                     </div>
-                    <div className="form-check">
-                    <label className="form-check-label">
-                        <input type="checkbox" className="form-check-input" name="optionsCheck" id="friday" value="friday"></input>
-                        F
-                      </label>
+                    <h5 className="text-center">Create an Alert (optional)</h5>
+                    <div className="row">
+                      <div className="col-md-6 col-xs-12">
+                        <div className="weekdays-selector form-group">
+                          <label htmlFor="light">Day(s) of the week</label><br></br>
+                          <input type="checkbox" id="weekday-mon" className="weekday" />
+                          <label htmlFor="weekday-mon">M</label>
+                          <input type="checkbox" id="weekday-tue" className="weekday" />
+                          <label htmlFor="weekday-tue">T</label>
+                          <input type="checkbox" id="weekday-wed" className="weekday" />
+                          <label htmlFor="weekday-wed">W</label>
+                          <input type="checkbox" id="weekday-thu" className="weekday" />
+                          <label htmlFor="weekday-thu">T</label>
+                          <input type="checkbox" id="weekday-fri" className="weekday" />
+                          <label htmlFor="weekday-fri">F</label>
+                          <input type="checkbox" id="weekday-sat" className="weekday" />
+                          <label htmlFor="weekday-sat">S</label>
+                          <input type="checkbox" id="weekday-sun" className="weekday" />
+                          <label htmlFor="weekday-sun">S</label>
+                        </div>
+                      </div>
+                      <div className="col-md-6 col-xs-12">
+                        <div className="form-group">
+                          <label htmlFor="frequency">Frequency</label>
+                          <select className="form-control" id="frequency-input">
+                            <option></option>
+                            <option>Every week</option>
+                            <option>Every other week</option>
+                            <option>Once every three weeks</option>
+                            <option>Once a Month</option>
+                          </select>
+                        </div>
+                      </div>
                     </div>
-                    <div className="form-check">
-                    <label className="form-check-label">
-                        <input type="checkbox" className="form-check-input" name="optionsCheck" id="saturday" value="saturday"></input>
-                        Sa
-                      </label>
-                    </div>
-                  </fieldset>
-                  <div className="form-group">
-                    <label for="frequency">Frequency</label>
-                    <select className="form-control" id="frequency-input">
-                      <option></option>
-                      <option>Every week</option>
-                      <option>Every other week</option>
-                      <option>Once every three weeks</option>
-                      <option>Once a Month</option>
-                    </select>
+                    <button type="submit" className="btn btn-primary">Submit</button>
                   </div>
-                  <button type="submit" className="btn btn-primary">Submit</button>
-                </form>
-              </div>          
+              </form>          
             </div>
           </div>
+        </div>
     );
   }
 });
--- app/components/Main.js
+++ app/components/Main.js
@@ -10,56 +10,55 @@ var Main = React.createClass({
   render: function() {
 
     return (
-        <div className="container-fluid">
-          
-          {/*header*/}
-          <div className="row" id="header">
-            <div className="col-xs-10">
-              <h1 className="navbar-left">Plant App</h1>
+        <div className="container-fluid" id="wrap">
+            {/*header*/}
+            <div className="row" id="header">
+              <div className="col-xs-10">
+                <h1 className="navbar-left">Plant App</h1>
+              </div>
+              <div className="col-xs-2">
+                <a href="#" className="navbar-right">LOGOUT</a>
+              </div>
             </div>
-            <div className="col-xs-2">
-              <a href="#" className="navbar-right">LOGOUT</a>
-            </div>
-          </div>
 
-          {/*Navigation*/}
-          <div className="row">
-            <div className="col-xs-1" id="navbar">
-              <div>
-                <Link to="/Alerts"><i className="fa fa-exclamation fa-2x" aria-hidden="true"></i></Link>
+            {/*Navigation*/}
+            <div className="row">
+              <div className="col-xs-1" id="navbar">
+                <div>
+                  <Link to="/Alerts"><i className="fa fa-exclamation fa-2x" aria-hidden="true"></i></Link>
+                </div>
+                <div>
+                  <Link to="/Calendar"><i className="fa fa-calendar fa-2x" aria-hidden="true"></i></Link>
+                </div>
+                <div>
+                  <Link to="/Plants"><i className="fa fa-leaf fa-2x" aria-hidden="true"></i></Link>
+                </div>
+                <div>
+                  <Link to="/AddPlant"><i className="fa fa-plus fa-2x" aria-hidden="true"></i></Link>
+                </div>
               </div>
-              <div>
-                <Link to="/Calendar"><i className="fa fa-calendar fa-2x" aria-hidden="true"></i></Link>
-              </div>
-              <div>
-                <Link to="/Plants"><i className="fa fa-leaf fa-2x" aria-hidden="true"></i></Link>
-              </div>
-              <div>
-                <Link to="/AddPlant"><i className="fa fa-plus fa-2x" aria-hidden="true"></i></Link>
-              </div>
-            </div>
             <div className="col-xs-11" id="content-wrapper">
               {/* Here we will deploy the sub components */}
               {/* These sub-components are getting passed as this.props.children */}
               {this.props.children}
             </div>
-
-            {/*footer*/}
-            <div className="row">
-              <footer className="footer">
-                <div className="col-xs-offset-4 col-xs-4">
-                  <p className="text-center">&copy; 2017 Plant App</p>
-                </div>
-                <div className="col-xs-4">
-                  <span className="pull-right">
-                    <a href="#"><i className="fa fa-info-circle" aria-hidden="true"></i></a>
-                    <a href="#"><i className="fa fa-github-alt" aria-hidden="true"></i></a>
-                  </span>
-                </div>
-              </footer>
-            </div>
-          </div>
         </div>
+
+          {/*footer*/}
+          <div className="row">
+            <footer className="footer">
+              <div className="col-xs-offset-4 col-xs-4">
+                <p className="text-center">&copy; 2017 Plant App</p>
+              </div>
+              <div className="col-xs-4">
+                <span className="pull-right">
+                  <a href="#"><i className="fa fa-info-circle" aria-hidden="true"></i></a>
+                  <a href="#"><i className="fa fa-github-alt" aria-hidden="true"></i></a>
+                </span>
+              </div>
+            </footer>
+          </div>
+      </div>
     );
   }
 });
