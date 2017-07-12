917526eab10ef87d7dd562709a6cf7fcb17f01c1
Theresa Medina <trgmedina@gmail.com>
2017-07-11 20:10:22 -0500

    created User model

---
+++ models/user.js
@@ -0,0 +1,30 @@
+// Include the Mongoose Dependencies
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+// Create a Schema for user info. 
+var UserSchema = new Schema({
+  firstname: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  username: {
+  	type: String,
+    required: true
+  },
+  password: {
+  	type: String,
+    required: true
+  }
+});
+
+// Create the Model
+var User = mongoose.model("User", UserSchema);
+
+// Export it for use elsewhere
+module.exports = User;
