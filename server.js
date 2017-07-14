5dbc969522322787eaa156fd10f091f4e5dd37d8
Joy Chang <chiaychang@users.noreply.github.com>
2017-07-13 20:04:58 -0500

    adding passport dependencies and user model

--- server.js
+++ server.js
@@ -1,8 +1,46 @@
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+
+require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({
+    secret: 'ilovescotchscotchyscotchscotch', // session secret
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
 // Include Server Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var flash = require('connect-flash');
+var passport = require('passport');
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // mongoose.Promise = Promise;
@@ -27,7 +65,7 @@ app.use(express.static("./public"));
 
 // MongoDB configuration (Change this URL to your own DB)
 // mongoose.connect("mongodb://heroku_6hktlx01:9r0nhq6bqf0cf7efnncpb4jtla@ds153732.mlab.com:53732/heroku_6hktlx01");
-mongoose.connect("mongodb://localhost/plantsdb")
+mongoose.connect("mongodb://localhost:27017/plantsdb")
 var db = mongoose.connection;
 
 db.on("error", function(err) {
