28c0956a1335cce53fe55956b850020c959b90d3
Theresa Medina <trgmedina@gmail.com>
2017-07-13 21:28:51 -0500

    Updating AddPlant.js component

--- app/components/AddPlant.js
+++ app/components/AddPlant.js
@@ -19,13 +19,29 @@ var AddPlant = React.createClass({
   },
 
   handleChange: function(event) {
-    const.target = event.target;
+    const target = event.target;
     const name = target.name;
 
     this.setState({
       [name]: value
     })
-  }
+  },
+
+  handleSubmit: function(event) {
+    event.preventDefault();
+
+    helpers.savePlant({
+      name: this.state.name,
+      description: this.state.description,
+      origin: this.state.origin,
+      sunlightAmt: this.state.sunlightAmt,
+      waterSchedule: this.state.waterSchedule,
+      imageURL: this.state.imageURL
+    })
+      .then(function() {
+        console.log("Posted to MongoDB");
+      })
+  },
 
   render: function() {
 
@@ -38,7 +54,7 @@ var AddPlant = React.createClass({
           </div>
           <div className="row">
               <div className="col-xs-10 col-xs-offset-1">
-                <form>
+                <form onSubmit={this.handleSubmit}>
                   <div id="addPlant-search">
                       <div className="input-group">
                         <input type="text" className="form-control" placeholder="Search for Plant Name..."></input>
@@ -48,18 +64,36 @@ var AddPlant = React.createClass({
                       </div>
                   </div>
                   <div id="addPlant-remaining">
+                    <div>
+                      <label>
+                        Name
+                        <input
+                          name="name" 
+                          type="text"
+                          className="form-control"
+                          id="nickname-input"
+                          onChange={this.handleChange}
+                          value={this.state.value}
+                          placeholder="Name of Plant (if new)">
+                        </input>
+                      </label>
+                    </div>
+                    <div>
                       <label>
                         Nickname
-                        <input 
+                        <input
                           name="nickname" 
                           type="text"
                           className="form-control"
                           id="nickname-input"
                           onChange={this.handleChange}
+                          value={this.state.value}
                           placeholder="Nickname">
                         </input>
                         <small id="nickname-help" className="form-text text-muted">We'll use this name for your plant if you prefer.</small>
                       </label>
+                    </div>
+                    <div>
                       <label>
                         Image URL
                         <input 
@@ -67,10 +101,14 @@ var AddPlant = React.createClass({
                           type="text" 
                           className="form-control" 
                           id="image-input" 
+                          onChange={this.handleChange}
+                          value={this.state.value}
                           placeholder="Image URL" 
-                          required
+                          required>
                         </input>
                       </label>
+                    </div>
+                    <div>
                       <label>
                         Description
                         <textarea
@@ -78,11 +116,15 @@ var AddPlant = React.createClass({
                           type="text"
                           className="form-control" 
                           id="description-input" 
+                          onChange={this.handleChange}
+                          value={this.state.value}
                           placeholder="Add Description Here"
                           rows="3"
-                          required
+                          required>
                         </textarea>
                       </label>
+                    </div>
+                    <div>
                       <label>
                         Plant Origin
                         <input 
@@ -90,32 +132,49 @@ var AddPlant = React.createClass({
                           type="text" 
                           className="form-control" 
                           id="origin-input" 
-                          placeholder="Origin"
+                          onChange={this.handleChange}
+                          value={this.state.value}
+                          placeholder="Origin">
                         </input>
                       </label>
+                    </div>
 
-                    <div className="row">x
+                    <div className="row">
                       <div className="col-md-6 col-xs-12">
                         <div className="form-group">
-                          <label htmlFor="light">Preferred Light</label>
-                          <select className="form-control" id="light-input">
-                            <option>Full Sun</option>
-                            <option>Partial Sun</option>
-                            <option>Bright Light</option>
-                            <option>Shade</option>
+                          <label>
+                            Preferred Light
+                            <select 
+                              name="sunlightAmt"
+                              className="form-control" 
+                              id="light-input" 
+                              onChange={this.handleChange}
+                              value={this.state.value}>
+                              <option value="Full Sun">Full Sun</option>
+                              <option value="Partial Sun">Partial Sun</option>
+                              <option value="Bright Light">Bright Light</option>
+                              <option value="Shade">Shade</option>
                           </select>
+                          </label>
                         </div>
                       </div>
                       <div className="col-md-6 col-xs-12">
                         <div className="form-group">
-                          <label htmlFor="water">Preferred Watering Frequency</label>
-                          <select className="form-control" id="water-input">
-                            <option>Once a Day</option>
-                            <option>Once a Week</option>
-                            <option>Every Other Week</option>
-                            <option>Once a Month</option>
-                            <option>Infrequent</option>
-                          </select>
+                          <label>
+                            Preferred Watering Frequency
+                            <select 
+                              name="waterSchedule"
+                              className="form-control" 
+                              id="water-input"
+                              onChange={this.handleChange}
+                              value={this.state.value}>
+                              <option value="Once a Day">Once a Day</option>
+                              <option value="Once a Week">Once a Week</option>
+                              <option value="Every Other Week">Every Other Week</option>
+                              <option value="Once a Month">Once a Month</option>
+                              <option value="Infrequent">Infrequent</option>
+                            </select>
+                          </label>
                         </div>
                       </div>
                     </div>
