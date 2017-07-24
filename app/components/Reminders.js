db87bc65861ce2f5605648b90e0dd0b4a0a82b70
Joy Chang <chiaychang@users.noreply.github.com>
2017-07-23 21:15:43 -0500

    ics working with latest event

--- app/components/Reminders.js
+++ app/components/Reminders.js
@@ -21,9 +21,9 @@ class Reminders extends React.Component {
     reminderHelpers.getReminders().then(function(reminderData) {
       // console.log("4. reminder data ", reminderData)
       this.setState({ reminders: reminderData });
+      reminderHelpers.getCalendar(reminderData);
     }.bind(this));
 
-    reminderHelpers.getCalendar(this.state.reminders);
    
     }
 
@@ -229,7 +229,7 @@ class Reminders extends React.Component {
             <div className="col-md-3"></div>
             <div className="col-md-6 text-center">
             <Popup
-                trigger={<a href="/plantr_calendar.ics" download="plantr_calendar.ics"><Button size='huge' icon='add' color='olive' content='Export to iCalendar'/></a>}
+                trigger={<a href="plantr_calendar.ics" download="plantr_calendar.ics"><Button size='huge' icon='add' color='olive' content='Export to iCalendar'/></a>}
                 content='download ics'
                 on='hover'
                 size='huge'
